d02ebbce2979873a0fb78ebdc01be655b7bc924a
INcode <vasconcelosthx@gmail.com>
2023-02-27 18:01:27 -0300

    teste de atualização

--- src/javaOop/BootCamp.java
+++ src/javaOop/BootCamp.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 import java.util.Set;
 
 public class BootCamp {
-
+//teste
     private String  nome;
     private String descricao;
     private final LocalDate dataInicial = LocalDate.now();
